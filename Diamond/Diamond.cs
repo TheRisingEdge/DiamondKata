81c631169c26f11b33eafdce49b9bd7e3977a4c8
Constantin Alupoaie <constantin.alupoaie@leaseplan.com>
2024-02-04 23:34:57 +0200

    pass failing tests for simple diamonds

--- Diamond/Diamond.cs
+++ Diamond/Diamond.cs
@@ -1,3 +1,5 @@
+using System.Text;
+
 namespace DiamondKata;
 
 public class Diamond
@@ -11,10 +13,68 @@ public class Diamond
 
     public override string ToString()
     {
-        var row = new char[2];
-        row[0] = _inputCharacter;
-        row[1] = '\n';
+        var topHalfCharacters = CharacterRange('A', _inputCharacter);
+        var bottomHalfCharacters = CharacterRange('A', _inputCharacter).Reverse().Skip(1);
+        var topToBottomCharacters = topHalfCharacters.Concat(bottomHalfCharacters);
 
-        return new string(row);
+        var (_, columns, midRowIndex) = MeasureDiamondSizeFor(_inputCharacter);
+
+        var diamondLines = topToBottomCharacters
+            .Select(c => PlaceCharacterOnLine(c, columns, midRowIndex));
+
+        return Stringify(diamondLines);
+    }
+
+    private (int rows, int columns, int midRowIndex) MeasureDiamondSizeFor(char inputCharacter)
+    {
+        var columns = (inputCharacter - 'A') * 2 + 1;
+        
+        return (columns, columns, columns / 2);
+    }
+
+    private char[] PlaceCharacterOnLine(char character, int lineSize, int lineMidIndex)
+    {
+        var characterMidOffset = 'A' - character;
+        var firstAppearanceIndex = lineMidIndex + characterMidOffset;
+        var secondAppearanceIndex = lineMidIndex - characterMidOffset;
+
+        var line = EmptyLine(lineSize);
+        line[firstAppearanceIndex] = character;
+        line[secondAppearanceIndex] = character;
+
+        return line;
+    }
+    
+    private static char[] EmptyLine(int size) 
+    {
+        var line = new char[size];
+        
+        for (var i = 0; i < size; i++)
+        {
+            line[i] = ' ';
+        }
+
+        return line;
+    }
+
+    private static IEnumerable<char> CharacterRange(char startCharacter, char endCharacter)
+    {
+        var count = endCharacter - startCharacter + 1;
+
+        return Enumerable.Range(startCharacter, count)
+            .Select(c => (char)c);
+    }
+
+    private static string Stringify(IEnumerable<char[]> diamondLines)
+    {
+        var sb = new StringBuilder();
+
+        foreach (var line in diamondLines)
+        {
+            sb.Append(line);
+            sb.Append('\n');
+        }
+
+        return sb.ToString();
     }
 }
