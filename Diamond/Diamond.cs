3a07e04d31fd74e8e2e4de9bc7723f066d6c1129
Constantin Alupoaie <constantin.alupoaie@leaseplan.com>
2024-02-04 21:12:24 +0200

    made test pass, throws on invalid characters

--- Diamond/Diamond.cs
+++ Diamond/Diamond.cs
@@ -2,7 +2,15 @@ namespace DiamondKata;
 
 public class Diamond
 {
+    private readonly char _inputCharacter;
+
     public Diamond(char inputCharacter)
     {
+        _inputCharacter = inputCharacter switch
+        {
+            >= 'A' and <= 'Z' => inputCharacter,
+            >= 'a' and <= 'z' => inputCharacter,
+            _ => throw new ArgumentException("Diamond can only be built for alphabet characters")
+        };
     }
 }
