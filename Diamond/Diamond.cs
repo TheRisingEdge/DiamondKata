0a21e6d74e3d6b3322bf3e51981cc106beaa58d5
Constantin Alupoaie <constantin.alupoaie@leaseplan.com>
2024-02-04 21:32:51 +0200

    pass test for A

--- Diamond/Diamond.cs
+++ Diamond/Diamond.cs
@@ -8,4 +8,13 @@ public class Diamond
     {
         _inputCharacter = char.IsAsciiLetter(inputCharacter) ? inputCharacter: throw new ArgumentException("Diamond can only be built for alphabet characters");
     }
+
+    public override string ToString()
+    {
+        var row = new char[2];
+        row[0] = _inputCharacter;
+        row[1] = '\n';
+
+        return new string(row);
+    }
 }
